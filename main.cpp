0c81e775e1a256cae7abf5273ab34c94c94895ac
AN99-dev <hoangand079@gmail.com>
2025-03-30 23:22:37 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -6,20 +6,21 @@
 #include <SDL_ttf.h>
 #include <SDL_mixer.h>
 #include <fstream>
-
+//khởi tạo thư viện
 
 using namespace std;
 
-const int SCREEN_WIDTH = 1200;
-const int SCREEN_HEIGHT = 750;
-const int PLAYER_SPEED = 3;
-const int JUMP_POWER = 10;
-const int GRAVITY = 1;
+const int SCREEN_WIDTH = 1200;// chiều dài màn hình
+const int SCREEN_HEIGHT = 750;//chiều cao màn hình
+const int PLAYER_SPEED = 3;//tốc đọ xuất phát của người chơi
+const int JUMP_POWER = 10;//khả năng bật nhảy của người chơi
+const int GRAVITY = 1;//hằng số trọng lực
 
-int Currentspeed= PLAYER_SPEED;
-bool soundFinished = false;
+int Currentspeed= PLAYER_SPEED;//tốc độ hiện tại của người chơi
+bool soundFinished = false;//kiểm tra điều kiện để tắt hết hiệu ứng âm thanh khi chơi và bật hiệu ứng thua cuộc
 
-
+//lớp người chơi gồm các thông số cơ bản như kích thước,tọa độ,ảnh của nhân vật,độ rơi của nhân vật,khả năng nhảy.
+//(không có tốc độ vì để thuận tiện cho code,ta cho các chướng ngại vật tiến gần về phía người chơi)
 class Player {
 public:
     int x, y, width, height;
@@ -60,6 +61,7 @@ public:
     }
 };
 
+//lớp chướng ngại vật bao gồm kích thước,tọa độ,và tốc độ tiến về phía người chơi
 class Obstacle {
 public:
     int x, y, width, height;
@@ -79,6 +81,8 @@ public:
     }
 };
 
+//hàm tái tạo chướng ngại vật khi nó đi qua hết màn hình
+
 void updateObstacles(vector<Obstacle>& obstacles) {
     for (size_t i = 0; i < obstacles.size(); i += 2) {
         obstacles[i].x -= Currentspeed;
@@ -92,7 +96,7 @@ void updateObstacles(vector<Obstacle>& obstacles) {
     }
 }
 
-
+// hàm hiểm thị điểm lên màn hình(ngoài ra còn để hiển thị thêm nhiều phần khác)
 
 void Renderscore(SDL_Renderer* renderer, TTF_Font* font, string Score,int x,int y) {
     SDL_Color color = {255, 255, 255, 255};
@@ -112,6 +116,7 @@ void Renderscore(SDL_Renderer* renderer, TTF_Font* font, string Score,int x,int 
     SDL_DestroyTexture(texture);
 }
 
+//hàm kiểm tra va chạm giữa nhân vật và chướng ngại vật
 bool checkCollision(const SDL_Rect& rect1, const SDL_Rect& rect2) {
     return rect1.x < rect2.x + rect2.w &&
            rect1.x + rect1.w > rect2.x &&
@@ -119,7 +124,7 @@ bool checkCollision(const SDL_Rect& rect1, const SDL_Rect& rect2) {
            rect1.y + rect1.h > rect2.y;
 }
 
-
+//hàm khởi tạo cửa sổ,tiêu đề
 bool initSDL(SDL_Window*& window, SDL_Renderer*& renderer) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         return false;
@@ -132,10 +137,12 @@ bool initSDL(SDL_Window*& window, SDL_Renderer*& renderer) {
     return renderer != nullptr;
 }
 
+//hàm kiểm tra nên dùng âm thanh gì
 void onSoundFinished(int channel) {
     soundFinished = true;
 }
 
+//hàm load ảnh
 SDL_Texture* loadTexture(const char* path, SDL_Renderer* renderer) {
     SDL_Surface* loadedSurface = IMG_Load(path);
     if (!loadedSurface) {
@@ -146,7 +153,7 @@ SDL_Texture* loadTexture(const char* path, SDL_Renderer* renderer) {
     return newTexture;
 }
 
-
+//hàm main
 int main(int argc, char* argv[]) {
     int score = 0;
     bool startGame = false;
@@ -171,6 +178,7 @@ int main(int argc, char* argv[]) {
     if (!sound){
         return -1;
     }
+    //khởi tạo các loại âm thanh
     Mix_Chunk* jumpSound = Mix_LoadWAV("jump.wav");
     if (!jumpSound){
         return -1;
@@ -192,13 +200,14 @@ int main(int argc, char* argv[]) {
     if (!initSDL(window, renderer)) return -1;
 
     IMG_Init(IMG_INIT_PNG);
-
+    //khởi tạo các loại hình ảnh
     SDL_Texture* backgroundTexture = loadTexture("background.png", renderer);
     SDL_Texture* playerTexture = loadTexture("player.png", renderer);
     SDL_Texture* obstacleTexture = loadTexture("Obstacle.png", renderer);
     if (!backgroundTexture || !playerTexture || !obstacleTexture){
         return -1;
     }
+    //khởi tạo nhân vật và chướng ngại vật
     Player player(300, SCREEN_HEIGHT - 400, playerTexture);
     vector<Obstacle> Obs = {
         Obstacle(SCREEN_WIDTH - 500, SCREEN_HEIGHT - 300, 300, obstacleTexture),
@@ -215,26 +224,28 @@ int main(int argc, char* argv[]) {
 
     bool quit = false;
     SDL_Event e;
-    int cameraX = 0;
+    int cameraX = 0;//biến carema di chuyển theo nhân vật
 
     while (!quit) {
+        //bắt đầu vòng lặp game
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT)
                 quit = true;
             else if (e.type == SDL_KEYDOWN) {
-                if (!startGame){
+                if (!startGame){//trước khi vào game cần nhấn enter
                     if(e.key.keysym.sym == SDLK_RETURN){
                         startGame = true;
                     }
                 }
-                else if (!gameOver) {
+                else if (!gameOver) {//kiểm tra xem người chơi đã thua chưa,
                     if (e.key.keysym.sym == SDLK_SPACE) {
-                        player.jump();
+                        player.jump();//nhấn space để nhảy lên
                         //Mix_PlayChannel(-1,jumpsound,0);
                     }
-                } else {
+                } else {//nếu người chơi đã thua cần nhấn r để mọi thứ bắt đầu lại
                     if (e.key.keysym.sym == SDLK_r) {
                          //Restart game
+                         //Mọi thứ được khởi tạo lại từ đầu
                         Mix_PlayChannel(-1, sound, -1);
                         gameOver = false;
                         score = 0;
@@ -260,6 +271,7 @@ int main(int argc, char* argv[]) {
             }
         }
         if (!startGame){
+            //trước khi bắt đầu game sẽ hiển thị mọi thứ kèm dòng chữ"Press Enter to start game"
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
             SDL_RenderClear(renderer);
             SDL_RenderCopy(renderer, backgroundTexture, NULL, NULL);
@@ -267,6 +279,7 @@ int main(int argc, char* argv[]) {
             for (auto& obs : Obs) {
                 obs.render(renderer, cameraX);
             }
+            //hiển thị score,highest score
             string Score = "Your score is: " + to_string(score);
             Renderscore(renderer, font, Score,50,50);
             string Highestscore = "Highest score is: " + to_string(highestscore);
@@ -276,21 +289,22 @@ int main(int argc, char* argv[]) {
             continue;
         }
         else if (!gameOver) {
+            //bắt đầu các hàm khi game chạy
             updateObstacles(Obs);
-            if (player.x > Obs[dem].x + Obs[dem].width) {
+            if (player.x > Obs[dem].x + Obs[dem].width) {//kiểm tra người đã di chuyển qua cột chưa
                 score++;
                 dem += 2;
-                if(score <= highestscore ||  demscore !=0){
+                if(score <= highestscore ||  demscore !=0){//chạy hiệu ứng ăn điểm nếu người chơi không phá kỷ lục cũ
                     Mix_PlayChannel(-1,Scoresound,0);
                 }
-                else{
+                else{//chạy hiệu ứng phá kỷ lục
                     Mix_PlayChannel(-1,Highscoresound,0);
                 }
                 if(score == highestscore+1){
                     demscore++;
                 }
                 if(score % 15 == 0 && Currentspeed <8 && score != 0) {
-                    Currentspeed++;
+                    Currentspeed++;//với mỗi 15 điểm người chơi đạt được,con chim sẽ tăng 1 tốc độ và tối đa là 8;
                 }  // Chuyển sang cặp chướng ngại vật tiếp theo
             if (dem >= Obs.size()) {
                 dem = 0;  // Quay lại từ đầu danh sách
@@ -306,17 +320,17 @@ int main(int argc, char* argv[]) {
             for (auto& obs : Obs) {
                 SDL_Rect playerRect = { player.x, player.y, player.width, player.height };
                 SDL_Rect obstacleRect = { obs.x, obs.y, obs.width, obs.height };
-
+                //nếu va chạm người chơi sẽ thua
                 if (checkCollision(playerRect, obstacleRect)) {
                     gameOver = true;
                     break;
-                }
+                }//nếu người chơi chạm đỉnh cửa sổ hoặc đáy cửa sổ đều tính là thua
                 if (player.y == SCREEN_HEIGHT - 50 || player.y == 0){
                     gameOver = true;
                     break;
                 }
             }
-            cameraX = player.x - SCREEN_WIDTH / 2;
+            cameraX = player.x - SCREEN_WIDTH / 2;//camera đuổi theo nhân vật
             if (cameraX < 0) cameraX = 0;
         }
         // Vẽ mọi thứ lên màn hình
@@ -327,21 +341,22 @@ int main(int argc, char* argv[]) {
         for (auto& obs : Obs) {
             obs.render(renderer, cameraX);
         }
-
+        //hiển thị điểm
         string Score = "Your score is: " + to_string(score);
         Renderscore(renderer, font, Score,50,50);
         string Highestscore = "Highest score is: " + to_string(highestscore);
         Renderscore(renderer,font,Highestscore,300,50);
         // Nếu game over, hiển thị "GAME OVER"
 
-        if (gameOver) {
+        if (gameOver) {//nếu thua
             if(musicback ==0){
-                Mix_HaltChannel(-1);
-                Mix_PlayChannel(-1,Gameoversound,0);
+                Mix_HaltChannel(-1);//dừng tất cả nhạc/hiệu ứng đang phát
+                Mix_PlayChannel(-1,Gameoversound,0);//phát nhạc thua
                 musicback ++;
             }
             Renderscore(renderer, font, "GAME OVER! Press 'R' to Restart.",50,100);
             ofstream file2;
+            //mở file ghi điểm cao nhất và ghi vào kết quả cao nhất của người chơi để khi thoát game điểm cao nhất vẫn được lưu cho lần chơi tiếp theo
             file2.open("Highestscore.txt");
             if (file2.is_open()) {
                 file2 << highestscore; // Ghi điểm mới
